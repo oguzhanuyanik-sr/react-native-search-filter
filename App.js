361755d9cf88de8f1e3fedb498c7533d0d21e9f0
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2024-06-03 12:54:09 +0300

    data fetching done

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, TextInput } from 'react-native';
 
+const API_ENDPOINT = `https://randomuser.me/api/?results=30`;
+
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [fullData, setFullData] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetchData(API_ENDPOINT);
+  }, []);
+
+  const fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json.results);
+
+      console.log(json.results);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    }
+  };
 
   const handleSearch = (query) => {
     setSearchQuery(query);
