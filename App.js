382a23fa5429b8d9f73e02d51fdda4dad65bfa59
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2024-06-03 12:47:34 +0300

    searchBox added.

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, TextInput } from 'react-native';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        marginHorizontal: 20,
+        marginTop: 50,
+      }}
+    >
+      <TextInput
+        placeholder='Search'
+        clearButtonMode='always'
+        style={styles.searchBox}
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={searchQuery}
+        onChangeText={(query) => handleSearch(query)}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  searchBox: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
   },
 });
